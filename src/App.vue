ce1f95fd5eb9c346e3e611595ea7217aff040350
Guilherme Santos Araújo <guilherme1984.araujo@gmail.com>
2023-01-26 21:14:24 -0300

    criacao, definindo fonte e chamando na tela principal, ola vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Olá vue!</h1>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url(./fonts/RobotoMono);
+}
+* {
+  font-family: "Roboto", monospace;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: #fff;
+  background: rgb(83, 105, 118);
+}
+</style>
